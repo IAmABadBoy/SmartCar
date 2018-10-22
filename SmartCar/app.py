bb0186d36c88516b920909ac0fb3bba55e14a809
IAmABadBoy <33645320+IAmABadBoy@users.noreply.github.com>
2018-10-22 10:13:18 +0800

    Add files via upload

--- SmartCar/app.py
+++ SmartCar/app.py
@@ -1,30 +1,47 @@
 from listen_ins import Client
+from simple_ctl import sctl
+from voice_ctl import vctl
 
 
-# main function
-def main():
-    listen_ins()
+class Controller:
+    def __init__(self,sctl:sctl,vctl:vctl):
+        self.mode = ''
+        self.sctl = sctl
+        self.vctl = vctl
+
+    def start(self):
+        thread = Client(1, 'Thread-1',self)
+        thread.start()
+        thread.join()
+
+    def voice_ctl(self, data):
+        self.vctl.get_data(data)
+
+    def auto_slam(self):
+        pass
+
+    def simple_ctl(self,data):
+        self.sctl.get_data(data)
+
+    def set_mode(self, mode):
+        self.mode = mode
+
+    def process(self, data):
+        if data == 'voice_ctl':
+            self.set_mode('voice_ctl')
+        elif data == 'auto_slam':
+            self.set_mode('auto_slam')
+        elif data == 'simple_ctl':
+            self.set_mode('simple_ctl')
+        elif data == 'main':
+            self.set_mode('main')
+        elif self.mode == 'voice_ctl':
+            self.voice_ctl(data)
+        elif self.mode == 'simple_ctl':
+            self.simple_ctl(data)
 
 
-def listen_ins():
-    thread = Client(1, 'Thread-1')
-    thread.start()
-    thread.join()
-
-
-def voice_ctl():
-    pass
-
-
-def auto_slam():
-    pass
-
-
-def simple_ctl():
-    pass
 
 
 if __name__ == '__main__':
-    main()
-
-
+    pass
--- SmartCar/listen_ins.py
+++ SmartCar/listen_ins.py
@@ -1,10 +1,12 @@
 from socket import *
+import abc
 import threading
 
 
 class Client(threading.Thread):
-    def __init__(self, threadID, name, port=9999, host='127.0.0.1'):
+    def __init__(self, threadID, name, controller, port=9999, host='127.0.0.1'):
         threading.Thread.__init__(self)
+        self.c=controller
         self.threadID = threadID
         self.name = name
         self.PORT = port
@@ -12,55 +14,15 @@ class Client(threading.Thread):
         self.s = socket(AF_INET, SOCK_DGRAM)
         self.s.bind((self.HOST, self.PORT))
         self.mode = ''
-        self.d_data = ''
-
-
-    def is_number(self,s):
-        try:
-            float(s)
-            return True
-        except ValueError:
-            pass
-
-        try:
-            import unicodedata
-            unicodedata.numeric(s)
-            return True
-        except (TypeError, ValueError):
-            pass
-
-        return False
 
     def run(self):
-        try:
-            while True:
+        while True:
+            try:
                 data, address = self.s.recvfrom(1024)
                 print(data, address)
                 d_data = data.decode()
-                self.d_data = d_data
-                if d_data == 'reset':
-                    self.mode = ''
-                    continue
-                if(self.mode == ''):
-                    if(d_data=='yykz'):
-                        self.s.sendto('yykz is open now'.encode(), address)
-                        self.mode = 'yykz'
-                    elif(d_data=='auto_slam'):
-                        self.s.sendto('auto_slam is open'.encode(), address)
-                        self.mode = 'auto_slam'
-                    elif(d_data=='simple_ctl'):
-                        self.s.sendto('simple_ctl is open'.encode(),address)
-                        self.mode = 'simple_ctl'
-                if(self.mode == 'yykz'):
-                    if(self.is_number(d_data)):
-                        pass
-                if(self.mode == 'auto_slam'):
-                    pass
-                if(self.mode == 'simple_cctl'):
-                    pass
-        except Exception as e:
-            print(e)
-    def get_mode(self):
-        return self.mode
-    def get_data(self):
-        return self.d_data
+                self.c.process()
+            except Exception as e:
+                print(e)
+
+
